d0438fae324fea2dba1824a3d2657b6c502c9748
lucien1011 <lucien1011@gmail.com>
2019-11-27 16:56:53 +0100

    Update ZX

--- ZX_10215/submitMC.sh
+++ ZX_10215/submitMC.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-python SubmitCrabJobs.py -t 10215_MCProd_191001 -d dataset_Legacy2016.txt -c templateMC_102X_Legacy16_2l_cfg.py 
+#python SubmitCrabJobs.py -t 10215_MCProd_191126 -d dataset_Legacy2016.txt -c templateMC_102X_Legacy16_2l_cfg.py 
+python SubmitCrabJobs.py -t 10215_MCProd_191127 -d dataset_Legacy2016.txt -c templateMC_102X_Legacy16_2l_cfg.py 
